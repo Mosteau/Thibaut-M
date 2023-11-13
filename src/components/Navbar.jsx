6fc3f4f76f1f633a705e3f656e54a155b0e5125a
Thibaut <118850152+Mosteau@users.noreply.github.com>
2023-11-13 14:14:08 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
       <h1 className='title'>Thibaut.M</h1>
       <ul className="nav-list">
         <li className="nav-item"><a href="./">Portfolio</a></li>
-        <li className="nav-item"><a href="public/CV 2023.pdf" download="mon-cv.pdf">Mon CV</a></li>
+        <li className="nav-item"><a href="../images/CV 2023.pdf" download="mon-cv.pdf">Mon CV</a></li>
         <li className="nav-item2" onClick={handleContactClick}>
           Contactez-moi
         </li>
