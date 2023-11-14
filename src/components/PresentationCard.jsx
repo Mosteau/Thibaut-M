b9f0aa94d8e4f54615bf39a420abb13eaa85dcde
Thibaut <118850152+Mosteau@users.noreply.github.com>
2023-11-14 12:19:17 +0100

    Update PresentationCard.jsx

--- src/components/PresentationCard.jsx
+++ src/components/PresentationCard.jsx
@@ -2,13 +2,7 @@ function PresentationCard() {
   return (
     <div className="presentation-card">
       <h2>Développeur Web FullStack</h2>
-      <p>{}
-        Développeur web junior passionné, avec une expérience commerciale et une soif d'apprendre.
-        <br />
-        <br />
-        Je crée des solutions numériques qui dépassent les attentes.
-        <br />
-        <br />
+      <p>
         Je suis un développeur web et mobile en formation, avec une passion pour la création de solutions numériques innovantes. Ma formation commerciale m'a donné une compréhension approfondie des besoins des clients, et ma soif d'apprendre me permet de rester à la pointe des dernières technologies.
         <br />
         <br />
