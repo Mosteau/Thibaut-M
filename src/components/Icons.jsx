8390f3cf916d1cd5b302591da55b1d10d1e923dc
Thibaut <118850152+Mosteau@users.noreply.github.com>
2023-11-14 12:15:06 +0100

    Update Icons.jsx

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -3,14 +3,14 @@
 function Icons() {
     return (
         <div className='icons'>
-            <img className='icon' src="../images/linux-logo.png" alt="linux" />
-            <img className='icon' src="../images/windows.png" alt="windows" />
-            <img className='icon' src="../images/html.png" alt="html" />
-            <img className='icon' src="../images/css.png" alt="css" />
-            <img className='icon' src="../images/js.png" alt="js" />
-            <img className='icon' src="../images/react.png" alt="react" />
-            <img className='icon' src="../images/mysql.png" alt="mysql" />
-            <img className='icon' src="../images/node-js.png" alt="node-js" />
+            <img className='icon' src="/images/linux-logo.png" alt="linux" />
+            <img className='icon' src="/images/windows.png" alt="windows" />
+            <img className='icon' src="/images/html.png" alt="html" />
+            <img className='icon' src="/images/css.png" alt="css" />
+            <img className='icon' src="/images/js.png" alt="js" />
+            <img className='icon' src="/images/react.png" alt="react" />
+            <img className='icon' src="/images/mysql.png" alt="mysql" />
+            <img className='icon' src="/images/node-js.png" alt="node-js" />
 
         </div>
     )
