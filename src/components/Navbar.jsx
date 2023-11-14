e44657b62ee1661ffa8190231579c8ca0f8ddef6
Thibaut <118850152+Mosteau@users.noreply.github.com>
2023-11-14 11:59:26 +0100

    test a element.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
       <h1 className='title'>Thibaut.M</h1>
       <ul className="nav-list">
         <li className="nav-item"><a href="./">Portfolio</a></li>
-        <li className="nav-item"><a href="../images/CV 2023.pdf" target="_blank">Mon CV</a></li>
+        <li className="nav-item"><a href="/images/CV 2023.pdf" target="_blank">Mon CV</a></li>
         <li className="nav-item2" onClick={handleContactClick}>
           Contactez-moi
         </li>
