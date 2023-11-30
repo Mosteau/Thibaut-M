79bfaed3fb88eea3f8d19f63a7f6f03a645e3890
Thibaut <thibaut.mosteau@lilo.org>
2023-11-30 13:53:20 +0100

    mise à jour projet 3

--- src/components/Projet3.jsx
+++ src/components/Projet3.jsx
@@ -1,7 +1,66 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+
 function Projet3() {
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState('');
+
+  const openModal = (imageSrc) => {
+    setSelectedImage(imageSrc);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <div className="projet-3">
-      <h2>Projet 3 -- A venir</h2>
+      <h2>Famille Inalti</h2>
+      <p>{`Site personnel sur les voyages en camping-car à travers l'Europe, nous partageons nos expériences et nos photos.`}</p>
+      <p>{`Site réalisé sur une base React JS.`}</p>
+      <a href="https://famille-inalti.vercel.app/" rel="noreferrer" target="_blank">Famille Inalti</a>
+      <div className='Images-RW3'>
+        <img
+          className="image-projets3"
+          src="/picturefi1.png"
+          alt="Image"
+          onClick={() => openModal("/radioworld1.jpg")}
+        />
+        <img
+          className="image-projets3"
+          src="/picturefi2.png"
+          alt="Image"
+          onClick={() => openModal("/radioworld2.jpg")}
+        />
+        <img
+          className="image-projets3"
+          src="/picturefi3.png"
+          alt="Image"
+          onClick={() => openModal("/radioworld3.jpg")}
+        />
+      </div>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Image Modal"
+        ariaHideApp={false}
+        className="custom-modal"
+      >
+        <button role="button" className="button-modale" onClick={closeModal}>
+          Fermer
+        </button>
+        {selectedImage && (
+          <div className="custom-modal__content">
+            <img
+              src={selectedImage}
+              alt="Image agrandie"
+            />
+          </div>
+        )}
+      </Modal>
     </div>
   );
 }
