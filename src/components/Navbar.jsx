7a4318e1c39d0ebe60af86bc064c96f2ababb346
Thibaut <thibaut.mosteau@lilo.org>
2023-12-02 22:47:25 +0100

    ajout link et scroll div

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import { bubble as Menu } from 'react-burger-menu';
+import { Link } from 'react-scroll';
 
 
 function Navbar() {
   const handleContactClick = () => {
     window.location.href = 'mailto:thibaut.mosteau@gmail.com';
   };
+  
 
   return (
     <div>
@@ -13,8 +15,48 @@ function Navbar() {
           <a href="https://github.com/Mosteau" rel="noreferrer" target="_blank">
             <img className="logo-git" src="/logo-github.png" alt="GitHub" />
           </a>
+          <Link
+            activeClass="active"
+            to="presentation-card"
+            spy={true}
+            smooth={true}
+            offset={-10}
+            duration={500}
+          >
+            Présentation
+          </Link>
           <a className="text-menu-burger" href="/CV2023.pdf" target="_blank">Mon CV</a>
           <a className="text-menu-burger" onClick={handleContactClick}>Contactez-moi</a>
+          <Link
+            activeClass="active"
+            to="projet-1"
+            spy={true}
+            smooth={true}
+            offset={-10}
+            duration={500}
+          >
+            Projet 1 - Spooky Clicker
+          </Link>
+          <Link
+            activeClass="active"
+            to="projet-2"
+            spy={true}
+            smooth={true}
+            offset={-10}
+            duration={500}
+          >
+            Projet 2 - Radion World
+          </Link>
+          <Link
+            activeClass="active"
+            to="projet-3"
+            spy={true}
+            smooth={true}
+            offset={-10}
+            duration={500}
+          >
+            Projet 3 - Famille Inalti
+          </Link>
         </Menu>
         <h1 className='titre-presentation'>Thibaut.M</h1>
       </nav>
