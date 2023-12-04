5b37881f9d67940dcb72030ae209d477d7731a8b
Thibaut <thibaut.mosteau@lilo.org>
2023-12-04 16:49:23 +0100

    modif titre p1é

--- src/components/Projet1.jsx
+++ src/components/Projet1.jsx
@@ -22,7 +22,7 @@ function Projet1() {
 
                 Pour la réalisation, nous avons travaillé à cinq sur le projet et utilisé les langages suivants : HTML, CSS et JavaScript pour la partie clic, Shop, animations et modales. La partie responsive a été faite pour joué sur smartphone.
     `}</p>
-            <p><a className="liens-sites"href="https://spookie-clicker.vercel.app" rel="noreferrer" target="_blank">---Spookie Clicer---</a>🎃</p>
+            <p><a className="liens-sites"href="https://spookie-clicker.vercel.app" rel="noreferrer" target="_blank">---Spookie Clicker---</a>🎃</p>
             <div className='Images-SP'>
                 <img
                     className="image-projets"
